38758c6e67b7ce8217a28ea21f9674830801dccd
Marcos M. Crocci <marcoscrocci@gmail.com>
2022-08-22 02:51:13 +0000

    Conexao com MySQL / Utilizacao do Remote-Container / Novas configuracoes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM node:14.17.0-slim
 
+RUN apt update && apt install -y wget netcat git
+
+#usuario root
+RUN wget -q -O /usr/bin/wait-for https://raw.githubusercontent.com/eficode/wait-for/v2.2.3/wait-for && \
+    chmod +x /usr/bin/wait-for
+
 USER node
 
 WORKDIR /home/node/app
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
-
 const app = express();
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'db',   // nome do servico
+    user: 'root',
+    password: 'root',
+    database: 'mydb'
+});
+
+connection.connect();
 
 app.get('/', (req, res) => {
     res.send('Executando Node.js dentro do Docker Compose v3!');
